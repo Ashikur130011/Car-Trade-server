92c02a12198c69b545b2815b3264c8267bc840ac
Ashikur Rahman <smashikurrahman24@gmail.com>
2021-11-12 14:10:52 +0600

    update

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ async function run() {
         if (user?.role === "admin") {
           isAdmin = true;
         }
-        res.json({ admin: isAdmin })
+        res.json(isAdmin)
       });
       //put admin
       app.put('/users/admin', async (req, res) => {
